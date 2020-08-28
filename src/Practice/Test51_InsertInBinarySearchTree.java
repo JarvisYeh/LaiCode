bb0acf73343305c6198cef2e24c91e7566d5ab9e
jarvis <jarvisyeh@qq.com>
2020-08-28 14:53:03 +0800

    Practice 7 - Binary Trees I

---
+++ src/Practice/Test51_InsertInBinarySearchTree.java
@@ -0,0 +1,50 @@
+package Practice;
+
+import util.TreeNode;
+
+public class Test51_InsertInBinarySearchTree {
+	// Solution 1: recursion
+	// if tree already has that key, do nothing
+	// always return the root of the new tree
+	public TreeNode insertI(TreeNode root, int key) {
+		if (root == null) {
+			return new TreeNode(key);
+		}
+
+		if (root.key < key) {
+			root.right = insertI(root.right, key);
+		} else if (root.key > key) {
+			root.left = insertI(root.left, key);
+		}
+		return root;
+	}
+
+	// Solution 2: iteration
+	// if tree already has that key, do nothing
+	// always return the root of the new tree
+	public TreeNode insertII(TreeNode root, int key) {
+		if (root == null) {
+			return new TreeNode(key);
+		}
+
+		TreeNode curr = root;
+		TreeNode prev = null;
+		while (curr != null) {
+			prev = curr;
+			if (curr.key < key) {
+				curr = curr.right;
+			} else if (curr.key > key) {
+				curr = curr.left;
+			} else {
+				return root;
+			}
+		}
+		if (prev.key < key) {
+			prev.right = new TreeNode(key);
+		} else {
+			prev.left = new TreeNode(key);
+		}
+		return root;
+	}
+
+}
---
+++ src/Practice/Test52_SearchInBinarySearchTree.java
@@ -0,0 +1,34 @@
+package Practice;
+
+import util.TreeNode;
+
+public class Test52_SearchInBinarySearchTree {
+	// Solution 1: recursion
+	public TreeNode searchI(TreeNode root, int key) {
+		if (root == null) {
+			return null;
+		}
+		if (root.key < key) {
+			return searchI(root.right, key);
+		} else if (root.key > key) {
+			return searchI(root.left, key);
+		} else {
+			return root;
+		}
+	}
+
+	// Solution 2: iteration
+	public TreeNode searchII(TreeNode root, int key) {
+		while (root != null) {
+			if (root.key > key) {
+				root = root.left;
+			} else if (root.key < key) {
+				root = root.right;
+			} else {
+				return root;
+			}
+		}
+		return root;
+	}
+
+}
