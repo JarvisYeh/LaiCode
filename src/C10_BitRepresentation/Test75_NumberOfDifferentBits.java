1a8d7d14e99805dc0ad562f6efb1022f7ab0fcb9
jarvis <jarvisyeh@qq.com>
2020-09-15 17:08:49 +0800

    Class 10 - Bit Representation

---
+++ src/C10_BitRepresentation/Test75_NumberOfDifferentBits.java
@@ -0,0 +1,13 @@
+package C10_BitRepresentation;
+
+public class Test75_NumberOfDifferentBits {
+	public int diffBits(int a, int b) {
+		int tmp = a ^ b;
+		int count = 0;
+		for (int i=0; i<32; i++) {
+			count += (tmp >> i & 1);
+		}
+		return count;
+
+	}
+}
