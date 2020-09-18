b788ad2dd2ce759bf54b659deb6b8d7602bf1af0
jarvis <jarvisyeh@qq.com>
2020-09-18 14:03:03 +0800

    Class 11 - Recursion 2

---
+++ src/C11_Recursion2/Test121_SpiralOrderTraverseI.java
@@ -0,0 +1,52 @@
+package C11_Recursion2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test121_SpiralOrderTraverseI {
+	// 螺旋打印N*N matrix
+	/**
+	 * 每次recursion打印一个圈
+	 * 圈分四次打印
+	 * size为当前圈的边长
+	 * offset为当前圈和最外圈的距离
+	 * 圈分四次打印时每次打印的数量为当前的size - 1
+	 **/
+	public List<Integer> spiral(int[][] matrix) {
+		List<Integer> res = new ArrayList<>();
+		spiral(res, matrix, 0, matrix.length);
+		return res;
+	}
+
+	private void spiral(List<Integer> res, int[][] mat, int offset, int size) {
+		if (size  <= 1) {
+			if (size == 1) {
+				res.add(mat[offset][offset]);
+			}
+			return;
+		}
+
+		// offset行打印[offset, offset + size  - 2]列
+		for (int i = 0; i <= size - 2; i++) {
+			res.add(mat[offset][offset + i]);
+		}
+
+		// mat.length - 1 - offset列打印[offset, offset + i]行
+		for (int i = 0; i <= size - 2; i++) {
+			res.add(mat[offset + i][mat.length - 1 - offset]);
+		}
+
+		// mat.length - 1 - offset行打印[mat.length - 1 - offset, mat.length - 1 - offset - i]列
+		for (int i = 0; i <= size - 2; i++) {
+			res.add(mat[mat.length - 1 - offset][mat.length - 1 - offset - i]);
+		}
+
+		// offset列打印[mat.length - 1 - offset]行
+		for (int i = 0; i <= size - 2; i++) {
+			res.add(mat[mat.length - 1 - offset - i][offset]);
+		}
+
+		spiral(res, mat, offset + 1, size - 2);
+	}
+
+}
---
+++ src/C11_Recursion2/Test292_StringAbbreviationMatching.java
@@ -0,0 +1,86 @@
+package C11_Recursion2;
+
+public class Test292_StringAbbreviationMatching {
+	/**
+	 * Time Complexity:
+	 * O(n^2)
+	 * Space Complexity:
+	 * O(n^2)
+	 **/
+	public boolean matchI(String text, String pattern) {
+		// base case
+		// 同时走完，说明之前的走匹配
+		if (text.length() == 0 && pattern.length() == 0) {
+			return true;
+		}
+		// 不同时走完，说明最后不匹配
+		if (text.length() == 0 || pattern.length() == 0) {
+			return false;
+		}
+
+		if (Character.isAlphabetic(pattern.charAt(0))) {
+			if (text.charAt(0) != pattern.charAt(0)) {
+				return false;
+			}
+			return matchI(text.substring(1), pattern.substring(1));
+		} else {
+			int i = 0;
+			int count = 0;
+			while (i < pattern.length() && Character.isDigit(pattern.charAt(i))) {
+				count = count * 10 + pattern.charAt(i) - '0';
+				i++;
+			}
+			if (count > text.length()) {
+				return false;
+			}
+			return matchI(text.substring(count), pattern.substring(i));
+		}
+	}
+
+
+	/**
+	 * 优化，不使用subString
+	 * Time Complexity:
+	 * O(n)
+	 * Space Complexity:
+	 * O(n)
+	 **/
+	public boolean matchII(String text, String pattern) {
+		return matchII(text, 0, pattern, 0);
+	}
+
+	public boolean matchII(String text, int textStart, String pattern, int patternStart) {
+		// base case
+		// 同时走完，说明之前的走匹配
+		if (text.length() == textStart && pattern.length() == patternStart) {
+			return true;
+		}
+		// 不同时走完，说明最后不匹配
+		if (text.length() == textStart || pattern.length() == patternStart) {
+			return false;
+		}
+
+		if (Character.isAlphabetic(pattern.charAt(patternStart))) {
+			if (text.charAt(textStart) != pattern.charAt(patternStart)) {
+				return false;
+			}
+			return matchII(text, textStart + 1, pattern, patternStart + 1);
+		} else {
+			int i = patternStart;
+			int count = 0;
+			while (i < pattern.length() && Character.isDigit(pattern.charAt(i))) {
+				count = count * 10 + pattern.charAt(i) - '0';
+				i++;
+			}
+			if (textStart + 1 + count > text.length()) {
+				return false;
+			}
+			return matchII(text, textStart + count, pattern, i);
+		}
+	}
+
+	public static void main(String[] args) {
+		Test292_StringAbbreviationMatching test = new Test292_StringAbbreviationMatching();
+		test.matchII("apple", "2p1");
+	}
+}
