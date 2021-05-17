39392023e61cf876d6435c50f760413c427d7c39
Jarvis Yeh <jarvisyeh@qq.com>
2021-05-17 19:37:41 +0800

    add recursion way of merge two sorted list

--- src/Algorithms/C3_LinkedList/Test40_MergeTwoSortedLinkedLists.java
+++ src/Algorithms/C3_LinkedList/Test40_MergeTwoSortedLinkedLists.java
@@ -6,7 +6,7 @@ public class Test40_MergeTwoSortedLinkedLists {
     /**
      * Use dummy head since the head is not certain, therefore there will be various of corner case
      **/
-    public ListNode mergeTwoSortedList(ListNode one, ListNode two) {
+    public ListNode mergeTwoSortedListI(ListNode one, ListNode two) {
         // corner case
         // actually could be deal as well, no need for corner case
         if (one == null) {
@@ -38,11 +38,31 @@ public class Test40_MergeTwoSortedLinkedLists {
         return dummyHead.next;
     }
 
+    // use recursion
+    public ListNode mergeTwoSortedListII(ListNode one, ListNode two) {
+        // base case
+        if (one == null) {
+            return two;
+        }
+        if (two == null) {
+            return one;
+        }
+
+        // recursive rule
+        if (one.value < two.value) {
+            one.next = mergeTwoSortedListII(one.next, two);
+            return one;
+        } else {
+            two.next = mergeTwoSortedListII(one, two.next);
+            return two;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode one = ListNode.fromArray(new int[]{1,3,5,7,9});
         ListNode two = ListNode.fromArray(new int[]{2,4,6,8,10});
         Test40_MergeTwoSortedLinkedLists test = new Test40_MergeTwoSortedLinkedLists();
-        ListNode.printAll(test.mergeTwoSortedList(one, two));
+        ListNode.printAll(test.mergeTwoSortedListII(one, two));
     }
 
 }
