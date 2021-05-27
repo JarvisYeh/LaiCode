21144a51595cca5abac9bbe81c83714ff874616f
Jarvis Yeh <jarvisyeh@qq.com>
2021-05-27 19:53:46 +0800

    add one alternative for complete tree check

--- src/Algorithms/C6_HeapAndGraphSeachAlgorithms1/Test47_CheckIfBinaryTreeIsCompleted.java
+++ src/Algorithms/C6_HeapAndGraphSeachAlgorithms1/Test47_CheckIfBinaryTreeIsCompleted.java
@@ -4,6 +4,8 @@ import util.TreeNode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Test47_CheckIfBinaryTreeIsCompleted {
 	/**
@@ -19,9 +21,9 @@ public class Test47_CheckIfBinaryTreeIsCompleted {
 	 * if flag == 1, return false
 	 * else continue;
 	 * The next time it meets some parent node whose child is not null
-	 * return flase
+	 * return false
 	 **/
-	public boolean isCompleted(TreeNode root) {
+	public boolean isCompletedI(TreeNode root) {
 		if (root == null) {
 			return true;
 		}
@@ -53,11 +55,35 @@ public class Test47_CheckIfBinaryTreeIsCompleted {
 		return true;
 	}
 
+	// level order traversal
+	// first met null, can not meet null again
+	// need to use linkedList as queue
+	public boolean isCompletedII(TreeNode root) {
+		if (root == null) return true;
+		boolean flag = false;
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while (queue.peek() != null) {
+			TreeNode curr = queue.poll();
+			queue.offer(curr.left);
+			queue.offer(curr.right);
+		}
+		// found the first null
+		// if the leftover in queue are all null, then it's complete tree
+		while (!queue.isEmpty()) {
+			if (queue.poll() != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
 	public static void main(String[] args) {
 		TreeNode root = TreeNode.formTreeByLayer(new Integer[]{3, null, 2});
 		Test47_CheckIfBinaryTreeIsCompleted test = new Test47_CheckIfBinaryTreeIsCompleted();
 
-		System.out.println(test.isCompleted(root));
+		System.out.println(test.isCompletedII(root));
 	}
 
 }
