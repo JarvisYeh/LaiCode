c35821f190f70a13d20f933d5570b5d7a1a37c54
Jarvis Yeh <jarvisyeh@qq.com>
2021-06-06 16:09:19 +0800

    Add one method for isPowerOfTwo()

--- src/Algorithms/C10_BitRepresentation/Test74_PowerOfTwo.java
+++ src/Algorithms/C10_BitRepresentation/Test74_PowerOfTwo.java
@@ -4,7 +4,7 @@ public class Test74_PowerOfTwo {
 	/**
 	 * 二进制表示中只有1位是1则是二的方
 	 **/
-	public boolean isPowerOfTwo(int number) {
+	public boolean isPowerOfTwoI(int number) {
 		if (number < 0) {
 			return false;
 		}
@@ -15,6 +15,14 @@ public class Test74_PowerOfTwo {
 			}
 		}
 		return count == 1 ? true : false;
+	}
+
+	/*
+	 *  01000 - 1 = 00111
+	 *  01000 & 00111 = 0
+	 */
+	public boolean isPowerOfTwoII(int number) {
+		return number > 0 && (number & (number - 1)) == 0;
 	}
 
 }
