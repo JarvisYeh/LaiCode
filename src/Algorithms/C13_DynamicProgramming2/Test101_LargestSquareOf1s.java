ab580f4f239acd6ac2260844a558562522e62f40
Jarvis Yeh <jarvisyeh@qq.com>
2021-06-14 15:18:30 +0800

    fix typo

--- src/Algorithms/C13_DynamicProgramming2/Test101_LargestSquareOf1s.java
+++ src/Algorithms/C13_DynamicProgramming2/Test101_LargestSquareOf1s.java
@@ -2,7 +2,7 @@ package Algorithms.C13_DynamicProgramming2;
 
 public class Test101_LargestSquareOf1s {
 	/**
-	 * M[i][j]代表以i, j为右下角的全1正方形最大变长
+	 * M[i][j]代表以i, j为右下角的全1正方形最大边长
 	 * Time Complexity: O(n^2)
 	 * Space Complexity: O(n^2)
 	 **/
