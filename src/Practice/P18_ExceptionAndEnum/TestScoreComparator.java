14c762391743021423e3b1297c47017e8630bf94
Jarvis Yeh <jarvisyeh@qq.com>
2021-06-21 14:30:22 +0800

    fix raw type to parameterized type

--- src/Practice/P18_ExceptionAndEnum/TestScoreComparator.java
+++ src/Practice/P18_ExceptionAndEnum/TestScoreComparator.java
@@ -6,9 +6,9 @@ public class TestScoreComparator {
 	public static void main(String[] args) {
 		Student s1 = new Student("abc", 100, 2, 3);
 		Student s2 = new Student("cde", 2, 3, 4);
-		Comparator mathComp = ScoreComparator.valueOf("MathComparator");
-		Comparator engComp = ScoreComparator.valueOf("EnglishComparator");
-		Comparator cnComp = LambdaScoreComparator.valueOf("ChineseComparator");
+		Comparator<Student> mathComp = ScoreComparator.valueOf("MathComparator");
+		Comparator<Student> engComp = ScoreComparator.valueOf("EnglishComparator");
+		Comparator<Student> cnComp = LambdaScoreComparator.valueOf("ChineseComparator");
 		System.out.println(mathComp.compare(s1, s2));
 		System.out.println(engComp.compare(s1, s2));
 		System.out.println(cnComp.compare(s1, s2));
