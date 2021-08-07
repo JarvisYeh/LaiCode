15e8ae4cef4e6ce347a09bf6dd7d178a09b27182
Jarvis Yeh <jarvisyeh@qq.com>
2021-08-07 13:14:50 +0800

    update array deduplication comment

--- src/Algorithms/C17_CrossTraining1/Test117_ArrayDeduplicationIII.java
+++ src/Algorithms/C17_CrossTraining1/Test117_ArrayDeduplicationIII.java
@@ -3,7 +3,7 @@ package Algorithms.C17_CrossTraining1;
 import java.util.Arrays;
 
 public class Test117_ArrayDeduplicationIII {
-	// sorted array每个保留一个
+	// sorted array去除所有重复字母
 	// 因为是sorted本身就不可能repeatedly removal
 	// 1233 => 12
 	public int[] deDup(int[] array) {
