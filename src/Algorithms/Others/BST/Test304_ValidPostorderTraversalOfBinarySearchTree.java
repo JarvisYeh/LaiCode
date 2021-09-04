edb350d2e874f84daf18ca75245081d6902b545e
Jarvis Yeh <jarvisyeh@qq.com>
2021-09-04 14:07:17 +0800

    update postorder traversal

--- src/Algorithms/Others/BST/Test304_ValidPostorderTraversalOfBinarySearchTree.java
+++ src/Algorithms/Others/BST/Test304_ValidPostorderTraversalOfBinarySearchTree.java
@@ -42,6 +42,7 @@ public class Test304_ValidPostorderTraversalOfBinarySearchTree {
 
 	private boolean checkValid(int[] post, int[] idx, int min, int max) {
 		if (idx[0] < 0) return true;
+
 		// left | right | root
 		// 3 5 4, 3 < 4, means that node is no longer in current RIGHT segment
 		if (post[idx[0]] < min) return true;
@@ -54,6 +55,31 @@ public class Test304_ValidPostorderTraversalOfBinarySearchTree {
 		int root = post[idx[0]];
 		idx[0]--;
 		return checkValid(post, idx, root, max) && checkValid(post, idx, min, root);
+	}
+
+	// Method 3
+	// TC: O(n)
+	// SC: O(h)
+	// traverse from post[n - 1] to post[0]
+	// check if it traversed all elements. if so, return true
+	public boolean validPostOrderIII(int[] post) {
+		if (post == null || post.length == 0) return true;
+		int[] idx = {post.length - 1};
+		traverse(post, idx, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return idx[0] == -1;
+	}
+
+	private void traverse(int[] post, int[] idx, int min, int max) {
+		if (idx[0] == -1) return;
+		if (post[idx[0]] <= min || post[idx[0]] >= max) return;
+
+		int root = post[idx[0]--];
+		if (idx[0] >= 0 && post[idx[0]] > root) {
+			traverse(post, idx, root, max);
+		}
+		if (idx[0] >= 0 && post[idx[0]] < root) {
+			traverse(post, idx, min, root);
+		}
 	}
 
 	public static void main(String[] args) {
