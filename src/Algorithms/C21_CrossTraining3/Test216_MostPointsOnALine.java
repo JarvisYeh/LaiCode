312430b04036fd7b8f2d867b076f86bc399ecf02
Jarvis Yeh <jarvisyeh@qq.com>
2023-09-28 01:45:18 +0800

    Add dealing method for special corner case w.r.t. MostPointsOnALine and fix typo

--- src/Algorithms/C21_CrossTraining3/Test216_MostPointsOnALine.java
+++ src/Algorithms/C21_CrossTraining3/Test216_MostPointsOnALine.java
@@ -55,13 +55,17 @@ public class Test216_MostPointsOnALine {
 				// update max
 				maxPoints = Math.max(maxPoints, pointsSet.size() + duplicates + 1);
 			}
+			// special corner case, all points have same values of x and y
+			if (duplicates + 1 == points.length) {
+				return points.length;
+			}
 		}
 		return maxPoints;
 	}
 
 	// y = kx + b, k = (y2 - y1)/(x2 - x1)
-	// y = (y2 - y1)/(x2 - x1)x + b => b = (x1x2 - y2x1)/(x2 - x1)
-	// y = (y2 - y1)/(x2 - x1)x + (x1x2 - y2x1)/(x2 - x1)
+	// y = (y2 - y1)/(x2 - x1)x + b => b = (y1x2 - y2x1)/(x2 - x1)
+	// y = (y2 - y1)/(x2 - x1)x + (y1x2 - y2x1)/(x2 - x1)
 	// at the same time:
 	// ax + by + c = 0
 	// we could get (y2 - y1)x + (x1 - x2)y + (y1x2 - y2x1)
@@ -76,30 +80,38 @@ public class Test216_MostPointsOnALine {
 		HashMap<String, HashSet<Point>> lineMap = new HashMap<>();
 		int maxPoints = 0;
 		for (Point p1 : points) {
+			int duplicates = 0;
 			for (Point p2 : points) {
-				if (p1.x != p2.x || p1.y != p2.y) {
-					// ax + by + c = 0
-					int a = p2.y - p1.y;
-					int b = p1.x - p2.x;
-					int c = p1.y*p2.x - p2.y*p1.x;
-					// normalize magnitude
-					int gcd = gcd(c, gcd(a, b));
-					a/=gcd;b/=gcd;c/=gcd;
-					// normalize sign
-					if (a < 0) {
-						a = -a; b = -b; c = -c;
-					} else if (a == 0 && b < 0) {
-						b = -b; c = -c;
-					}
-					String line = a + "," + b + "," + c;
-					// retrieve point set for corresponding line
-					HashSet<Point> pointsSet = lineMap.getOrDefault(line, new HashSet<Point>());
-					pointsSet.add(p1);
-					pointsSet.add(p2);
-					lineMap.put(line, pointsSet);
-					// update max
-					maxPoints = Math.max(maxPoints, pointsSet.size());
+				// same point, update duplicates
+				if (p1.x == p2.x && p1.y == p2.y) {
+					duplicates++;
+					continue;
 				}
+				// ax + by + c = 0
+				int a = p2.y - p1.y;
+				int b = p1.x - p2.x;
+				int c = p1.y*p2.x - p2.y*p1.x;
+				// normalize magnitude
+				int gcd = gcd(c, gcd(a, b));
+				a/=gcd;b/=gcd;c/=gcd;
+				// normalize sign
+				if (a < 0) {
+					a = -a; b = -b; c = -c;
+				} else if (a == 0 && b < 0) {
+					b = -b; c = -c;
+				}
+				String line = a + "," + b + "," + c;
+				// retrieve point set for corresponding line
+				HashSet<Point> pointsSet = lineMap.getOrDefault(line, new HashSet<Point>());
+				pointsSet.add(p1);
+				pointsSet.add(p2);
+				lineMap.put(line, pointsSet);
+				// update max
+				maxPoints = Math.max(maxPoints, pointsSet.size());
+			}
+			// special corner case, all points have same values of x and y
+			if (duplicates == points.length) {
+				return points.length;
 			}
 		}
 		return maxPoints;
